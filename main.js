05103bfe71f4d890181e0510b924a2008958195e
m-bylac <k1810154@komajo.ac.jp>
2021-12-10 01:08:34 +0900

    first-commit

---
+++ main.js
@@ -0,0 +1,20 @@
+function leapYear(year){
+    if( (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+      return true;
+    }
+    return false;
+}
+
+let checkYear = 2021;
+if(leapYear(checkYear)){
+    console.log(checkYear + '年はうるう年です');
+}else{
+    console.log(checkYear + '年はうるう年ではありません');
+}
+
+const array = [2, 4, 7, 5, 4, 3, 8];
+
+const new_array = array.filter(function(val, i, array){
+	return (array.indexOf(val) === i);
+});
+console.log(new_array);
